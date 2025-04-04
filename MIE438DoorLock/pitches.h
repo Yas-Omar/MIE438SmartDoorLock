765c473fc2e8dc6d4448e6557fec0a416eae7ae7
Yasin Omar <yasin.omar2@outlook.com>
2025-04-03 23:46:16 -0400

    Create pitches.h

---
+++ MIE438DoorLock/pitches.h
@@ -0,0 +1,12 @@
+/*************************************************
+ SPECIFY VALUES FOR NOTE PLAYING (MELODY)
+ *************************************************/
+#define NOTE_F4 349
+
+#define NOTE_G4 392
+
+#define NOTE_A4 440
+
+#define NOTE_AS4 466
+
+#define NOTE_C4 262
