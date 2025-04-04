73611ee02a4e54f33cfc539ee80913fe13e16cbc
Yasin Omar <yasin.omar2@outlook.com>
2025-04-03 23:46:59 -0400

    should be the final code fingers crossed

--- MIE438DoorLock/smartDoorLock.ino
+++ MIE438DoorLock/smartDoorLock.ino
@@ -1,10 +1,48 @@
-
+#include pitches.h
 //#include "esp32_camera.ino"  --> Used if library is not part of current sketch
 // 
 faceDetection doorScan;
-gestureDetection gestScan;
+gestureDetection doorScan;
 
 //-------------- Def
+
+#define PIN_SPEAKER = 16;
+
+int ringMelody[] = {
+  NOTE_F4, 
+  NOTE_G4, 
+  NOTE_A4, 
+  NOTE_G4,
+  NOTE_A4, 
+  NOTE_AS4,
+  NOTE_A4, 
+  NOTE_G4,
+  NOTE_F4, 
+  NOTE_G4,
+  NOTE_A4, 
+  NOTE_C4,
+  NOTE_C4,
+  NOTE_C4,
+  NOTE_C4,
+  NOTE_C4
+};
+
+int ringDuration[] = {
+  4, 4, 8, 4, 8, 4, 4,
+  8, 4, 8, 4, 8, 4, 8,
+  4,1
+};
+
+void playRingMelody(){
+  for (int i  =  0; i!=16; i++){
+    int noteDuration =  (2000)/ringDuration[i];
+    tone(PIN_SPEAKER, ringMelody[i], noteDuration*0.95);
+    delay(noteDuration);
+    noTone(PIN_SPEAKER);
+  }
+
+}
+
 enum State{
   IDLE,
   SCANNING,
@@ -30,7 +68,7 @@ Motor_Substate lastSubstate = LOCKING;
 
 
 const char* userLabel = "Baqir", *ownerLabel = "Yasin";
-const char* userGestures[2] = {"Peace", "OpenPalm"};
+const char* gestureRec  = "Wave";
 
 
 void setup(){
@@ -79,30 +117,21 @@ void loop(){
           //Gesture recog
           ei_printf("Owner Detected, scanning for appropriate gesture \r\n");
           if (cameraCheck == 0){
-            gestScan.cameraInit();
+            doorScan.cameraInit();
             delay(2000);
             cameraCheck = 1;
           }
-          gestScan.runImpulse();
-          if (strcmp(gestScan.label, userGestures[0]) == 0){
+          doorScan.runImpulse();
+          if (strcmp(doorScan.label, gestureRec) == 0){
             if (lastSubstate != UNLOCKING){
               lockState = MOTOR_HANDLER;
               motorPhase = UNLOCKING;
               cameraCheck = 0;
             }
             else{
-              ei_printf("DOOR IS ALREADY UNLOCKED, PLEASE LOCK BEFORE ATTEMPTING TO UNLOCK \r\n");
-            }
-
-          }
-          else if (strcmp(gestScan.label, userGestures[1] == 0)){
-            if (lastSubstate != LOCKING){
-              lockState = MOTOR_HANDLER;
-              motorPhase =  LOCKING;
+              lockstate = MOTOR_HANDLER;
+              motorPhase = LOCKING;
               cameraCheck = 0;
-            }
-            else{
-              ei_printf("DOOR IS ALREADY LOCKED, PLEASE UNLOCK BEFORE ATTEMPTING TO LOCK \r\n");
             }
           }
           delay(1000);
@@ -112,14 +141,15 @@ void loop(){
           //Gesture recog
           ei_printf("Special Permission Substate \r\n");
           if (cameraCheck == 0){
-            gestScan.cameraInit();
+            doorScan.cameraInit();
             delay(2000);
             cameraCheck = 1;
           }
-          gestScan.runImpulse();
-          if (strcmp(gestScan.label, userGestures[0])){
-            lockState = MOTOR_HANDLER;   // Call to Speaker State will go here.
-            cameraCheck = 0;
+          doorScan.runImpulse();
+          if (strcmp(doorScan.label, gestureRec) == 0){
+            playRingMelody();
+            delay(5000);
+            lockState = SCANNING;
           }
           delay(1000);
           break;
